23158cd39c84350674734eed8e4155cc33937440
Roberto Alvarez <roberto_utb@hotmail.com>
2019-01-14 13:38:21 -0600

    small changes on both server and web side

--- LagoonBackend/src/main/java/com/lagoon/LagoonBackend/controller/PhotoController.java
+++ LagoonBackend/src/main/java/com/lagoon/LagoonBackend/controller/PhotoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/photos")
+@RequestMapping("/photo")
 public class PhotoController {
 
     @Autowired
